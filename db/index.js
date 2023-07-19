7ad448fec9ee8f4ff40f8e692a705be92614675f
Ayoze <ayose89@gmail.com>
2023-07-19 22:16:13 +0100

    fixeed

--- db/index.js
+++ db/index.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 // Conexión a la base de datos
 const connectDB = async () => {
 mongoose
-  .connect(process.env.URL_CONNECT, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.URL_CONNECT)
   .then(() => {
     console.log("Conexión exitosa a MongoDB");
   })
